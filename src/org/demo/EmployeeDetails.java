05c43939e6088b4f023fe11aa815fdf891045be4
navanithat <navanithat30@gmail.com>
2024-01-10 08:27:40 -0800

    Employee detail verified

---
+++ src/org/demo/EmployeeDetails.java
@@ -0,0 +1,17 @@
+package org.demo;
+
+public class EmployeeDetails {
+	
+	private void EmployeeName() {
+		System.out.println("Employee name is Raja");
+	}
+
+	private void EmployeeId() {
+		System.out.println("Employee id is 123");
+
+	}
+	
+	private void Employeedob() {
+		System.out.println("Employee dob is 6 Jan 1997");
+	}
+}
